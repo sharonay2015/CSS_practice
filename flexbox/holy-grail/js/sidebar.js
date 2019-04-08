356293651e4d393ef4d43eb9cf6657227838f581
sharonay2015 <youngs_e@hotmail.com>
2019-04-07 20:39:36 -0400

    added jQuery to justify toggle button

--- flexbox/holy-grail/js/sidebar.js
+++ flexbox/holy-grail/js/sidebar.js
@@ -10,8 +10,15 @@ $('a.sidebarRightToggle').click(function () {
   console.log("right toggle");
   if ($('.sidebar').hasClass('sidebarLeft')) {
     $('.sidebar').removeClass('sidebarLeft');
-
   };
 });
 
+$('a.noSidebarToggle').click(function () {
+  console.log("no sidebar toggle");
+  if (!$('.content').hasClass('noSidebar')) {
+    $('.content').addClass('noSidebar');
+  } else {
+    $('.content').removeClass('noSidebar');
+  };
+});
 
