05bc1df068472b18bbfab312555e40ad4caaf434
sharonay2015 <youngs_e@hotmail.com>
2019-04-07 21:07:07 -0400

    added animation to button functions

--- flexbox/holy-grail/js/sidebar.js
+++ flexbox/holy-grail/js/sidebar.js
@@ -1,18 +1,18 @@
-
+// show side bar on left
 $('a.sidebarLeftToggle').click(function () {
   if (!$('.sidebar').hasClass('sidebarLeft')) {
     $('.sidebar').addClass('sidebarLeft');
     console.log("left toggle");
   };
 });
-
+// show side bar on right
 $('a.sidebarRightToggle').click(function () {
   console.log("right toggle");
   if ($('.sidebar').hasClass('sidebarLeft')) {
     $('.sidebar').removeClass('sidebarLeft');
   };
 });
-
+//show sidebar below content, click button again to remove justify
 $('a.noSidebarToggle').click(function () {
   console.log("no sidebar toggle");
   if (!$('.content').hasClass('noSidebar')) {
@@ -21,4 +21,12 @@ $('a.noSidebarToggle').click(function () {
     $('.content').removeClass('noSidebar');
   };
 });
-
+//hide sidebar, click button again to reshow sidebar
+$('a.hideSidebarToggle').click(function () {
+  console.log("hide toggle");
+  if (!$('.sidebar').hasClass('hide')) {
+    $('.sidebar').addClass('hide');
+  } else {
+    $('.sidebar').removeClass('hide');
+  };
+});
